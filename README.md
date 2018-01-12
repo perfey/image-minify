e3e255aecf5fa9b352b98448f6b07b4eadc7219f
damonpfliu <damonpfliu@tencent.com>
2018-01-12 09:47:05 +0800

    功能实现

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # image-minify
 
+图片压缩  
+Base on [Imagine@0.4.0](https://github.com/meowtec/Imagine "Imagine@0.4.0")  
+
+## 安装
+```shell
+npm install image-minify --save-dev
+```
+
+## 使用
+代码示例
+
+```javascript
+const minify = require('image-minify')
+
+// const imgSpawn = minify.pngSpawn(
+//   './xxx.png',
+//   './xxx.min.png'
+// )
+
+// const imgSpawn = minify.pngSpawn(
+//   './xxx.png',
+//   './xxx.min.webp'
+// )
+
+const imgSpawn = minify.jpgSpawn(
+  './xxx.jpg',
+  './xxx.min.jpg'
+)
+
+imgSpawn.stdout.on('data', function (data) {
+  console.info('imgSpawn stdout: ', String(data))
+})
+imgSpawn.stderr.on('data', function (data) {
+  console.error('imgSpawn stderr: ', String(data))
+})
+imgSpawn.on('close', function (code) {
+  console.error('imgSpawn code: ', code)
+})
+```
+
+
+参数说明
+
+```javascript
+pngSpawn(
+	input, // 源文件地址
+	output, // 生成文件地址
+	options // 配置项 对象，默认值 { color = 256 }
+)
+```
+
+```javascript
+jpgSpawn(
+	input, // 源文件地址
+	output, // 生成文件地址
+	options // 配置项 对象，默认值 { quality = 70 }
+)
+```
+
+```javascript
+webpSpawn(
+	input, // 源文件地址
+	output, // 生成文件地址
+	options // 配置项 对象，默认值 { quality = 80 }
+)
+```
--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-module.export = {}
+const path = require("path")
+const os = require("os")
+const spawn = require("child_process").spawn
+
+const platformNames = {
+  darwin: 'mac',
+  win32: 'win',
+}
+const platform = os.platform()
+const targetDir = platformNames[platform] || platform
+const getBin = (name) => {
+  if (platform === 'win32') {
+    name = name + '.exe'
+  }
+  return path.resolve(__dirname, './bin', targetDir, name)
+}
+
+const pngquant = getBin('pngquant')
+const mozjpeg = getBin('moz-cjpeg')
+const cwebp = getBin('cwebp')
+
+exports.pngSpawn = (input, output, options) => {
+  options = options || {}
+  const { color = 256 } = options
+  const spawnArgs = [
+      color.toString(),
+      input,
+      '-o',
+      output,
+  ]
+  return spawn(pngquant, spawnArgs)
+}
+exports.jpgSpawn = (input, output, options) => {
+  options = options || {}
+  const { quality = 70 } = options
+  const spawnArgs = [
+      '-quality',
+      quality.toString(),
+      '-outfile',
+      output,
+      input,
+  ]
+  return spawn(mozjpeg, spawnArgs)
+}
+exports.webpSpawn = (input, output, options) => {
+  options = options || {}
+  const { quality = 80 } = options
+  const spawnArgs = [
+      '-q',
+      quality.toString(),
+      input,
+      '-o',
+      output,
+  ]
+  return spawn(cwebp, spawnArgs)
+}
